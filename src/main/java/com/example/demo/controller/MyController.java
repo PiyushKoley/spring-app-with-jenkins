e27e8a7431e0ebe46fe9e982b05716058e1e7695
Piyush Koley <piyush.koley@kirtitec.com>
2024-04-23 17:06:55 +0530

    first commit with /check end-point

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.WebSocket;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/check")
+    public ResponseEntity<?> check() {
+        return ResponseEntity.ok("up and running");
+    }
+
+}
